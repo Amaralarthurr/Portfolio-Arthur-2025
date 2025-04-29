e1fce9e344dafc521bb1bf183096c5de13e97482
Arthur <arthuramaral697@gmail.com>
2025-04-29 14:13:40 -0300

    projects folder delete

--- src/projects/SPRINT-3-main/js/balao.js
+++
@@ -1,6 +0,0 @@
-function toggleBalao() {
-  const balao = document.getElementById("balao-cicquinho");
-  balao.classList.toggle("mostrar");
-}
-
-setInterval(toggleBalao, 10000); // O balão vai aparecer e desaparecer a cada 10 segundos
